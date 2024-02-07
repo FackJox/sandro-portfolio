06d82b41e1e083e3a0386f1bf25fea5eaaa045e6
FackJox <jackfox@tuta.io>
2024-02-07 13:32:07 +0700

    carousel fixing

--- app/api/imageproxy.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-    const { imageUrl } = req.query;
-    const response = await fetch(imageUrl);
-    const imageBuffer = await response.arrayBuffer();
-    res.setHeader('Content-Type', 'image/jpeg');
-    res.send(Buffer.from(imageBuffer));
-}
